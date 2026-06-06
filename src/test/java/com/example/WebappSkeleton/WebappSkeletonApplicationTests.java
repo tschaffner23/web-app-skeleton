c1f18ba1d356057f39d6368b2fff97d26ea08260
Taylor Schaffner <tschaffner23@gmail.com>
2026-06-06 15:36:38 -0500

    Generated base files from Spring.io

---
+++ src/test/java/com/example/WebappSkeleton/WebappSkeletonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WebappSkeleton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappSkeletonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
